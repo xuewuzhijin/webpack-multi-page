914777b7e76a10278435ad0fd8f1c3f551d7956d
bill <1311409079@qq.com>
2019-11-25 23:32:05 +0800

    update minSize

---
+++ views/home/index.ts
@@ -0,0 +1,4 @@
+import "@styles/common.styl";
+import $ from "jquery"
+console.log($)
+console.log("this is home ts")
---
+++ webpack.config.dev.ts
@@ -0,0 +1,57 @@
+import path from "path"
+import webpack, { Configuration } from "webpack"
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import Glob from "glob"
+
+// 配置多入口插件
+const plugins: webpack.Plugin[] = [];
+const reg = /.*\/(.+)\/(.+)\.(\w+)$/;
+Glob.sync("./views/**/index.ts").forEach( item => {
+  const matchs = item.match(reg) as string[],
+        name = matchs[1];
+  plugins.push(
+    new HtmlWebpackPlugin({
+      filename: name + ".html",
+      template: item.replace(/(\.ts)/, ".html"),
+      inject: true,
+      chunks: [ name ]
+    }))
+})
+
+module.exports = {
+  mode: "development",
+
+  devtool: "#@eval-source-map",
+
+  output: {
+    filename: "js/[name].js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader", "postcss-loader"]
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
+          "postcss-loader",
+          {
+            loader: "stylus-loader",
+            options: {
+              import: path.resolve( __dirname, "public/styles/env.styl" )
+            }
+          }
+        ]
+      },
+    ]
+  },
+
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ].concat(plugins)
+} as Configuration
