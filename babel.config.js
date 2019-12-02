b3b9c2533e25c33dbcb9b606e7737ce3f4644ed5
bill <1311409079@qq.com>
2019-12-02 17:58:32 +0800

    优化语法，解决jsx/tsx问题

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,26 @@
-const Config = require("./deploy/config.js");
-
-const babel = {
+const Config = require("./deploy/config.js"),
+      babel = {
 	"presets": [
     // 编译环境设置，自动注入 polyfill
     [ "@babel/preset-env", { useBuiltIns: "usage", modules: false, corejs: { version: 3, proposal: true } } ],
-    "@babel/typescript"
+    [ "@babel/preset-typescript" ]
 	],
 	"plugins": [
     "@babel/plugin-transform-runtime",
-    ["@babel/plugin-proposal-decorators", { "legacy": true }],
-    ["@babel/plugin-proposal-class-properties", { "loose" : true }],
-    ["@babel/plugin-transform-arrow-functions", { "spec": true }],
-    "@babel/plugin-proposal-object-rest-spread",
     "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-object-rest-spread",
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-transform-arrow-functions", { "spec": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose" : true }],
+    ["@babel/plugin-transform-typescript", { allowNamespaces: true }]
 	]
 };
 
 if ( Config.plugin.react ) {
   // 如果开启 react，使用 .jsx/.tsx 编译设置
   babel.presets.push("@babel/preset-react")
+  // 强制解决 jsx 问题
+  babel.presets[1][2] = { allExtensions: true, isTsx: true }
   // 并且使用 react 的热更新插件( webpack 自带的热更新会导致模块更新后数据丢失 )
   babel.presets.push("react-hot-loader/babel")
 }
