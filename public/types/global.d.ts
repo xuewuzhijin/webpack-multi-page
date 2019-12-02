cf435352352ad883e732e4c88374e985006143b3
bill <1311409079@qq.com>
2019-12-02 18:10:18 +0800

    解决项目公共配置和类型定义目录为空，并默认增加ts对vue文件的支持，以及配置全局Window的属性示例

---
+++ public/types/global.d.ts
@@ -0,0 +1,6 @@
+declare global {
+  // 全局定义 window 属性
+  interface Window {}
+}
+
+export {};
---
+++ public/types/vue.d.ts
@@ -0,0 +1,6 @@
+import Vue from "vue";
+
+declare module "vue/types/vue" {
+  /** 给 Vue 定义 prototype 属性 */
+  interface Vue {}
+}
