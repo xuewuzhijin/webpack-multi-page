0879420923b07425f003035a6ac44afcb9e85332
bill <1311409079@qq.com>
2019-12-02 18:00:15 +0800

    优化语法

--- deploy/_util/index.ts
+++ deploy/_util/index.ts
@@ -5,20 +5,24 @@ import { Configuration } from "webpack"
 import WebpackMerge from "webpack-merge";
 
 /** 返回的入口文件数组
- * @param { Function } cb 回调函数
- * @param { String } path 返回相对于项目根目录的相对路径
- * @param { String } name 返回截取后的名称作为 chunk 名称
- * @param { String } templatePath 用于判断路径的入口文件有没有对应名称的 html 文件
+ * @param { function } cb 回调函数
+ * @param { string } path 返回相对于项目根目录的相对路径
+ * @param { string } name 返回截取后的名称作为 chunk 名称
+ * @param { string } templatePath 用于判断路径的入口文件有没有对应名称的 html 文件
  */
 const entrys = ( cb: (path: string, name: string, templatePath: string) => void ) => {
 
-  Glob.sync(`{./views/@(${Config.entry.name.join("|")}).[jt]s,./views/**/!(${Config.entry.ignorePrefix.join("|")})*/@(${Config.entry.name.join("|")}).[jt]s}`)
+  let { entry: { name, ignorePrefix } } = Config,
+      newName = name.join("|");
+
+  Glob.sync(`{./views/@(${newName}).[jt]s,./views/**/!(${ignorePrefix.join("|")})*/@(${newName}).[jt]s}`)
   .forEach( path => {
 
   let name = path.replace(new RegExp(`(./views/)|(\.[jt]s)`, "g"), ""),
       temp = path.replace(/[jt]s$/, "html"),
       // 判断文件是否存在，如果不存在则使用默认的模版文件
       exist= fs.existsSync( temp );
+      
   cb( path, name + ".html", exist ? temp : Config.templatePath )
 
   })
@@ -29,7 +33,7 @@ type PluginKey = keyof typeof Config.plugin;
 /** 返回使用的插件列表 */
 let webpack: Configuration = {};
 for (const key in Config.plugin) {
-  Config.plugin[(key as PluginKey)] && (webpack = WebpackMerge(webpack, require(`./plugins/${key}`)));
+  Config.plugin[ (<PluginKey> key) ] && (webpack = WebpackMerge(webpack, require(`./plugins/${key}`)));
 }
 
 
